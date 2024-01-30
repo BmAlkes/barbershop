bc57bd8c3757132d3bf904911532d00d388ffef0
bmalkes <bmalkes@gmail.com>
2024-01-30 18:34:28 +0200

    feat:add search initial component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,19 @@
+import { format } from "date-fns";
 import Image from "next/image";
+import Search from "./_component/search";
 
 export default function Home() {
-  return <main>fsw</main>;
+  return (
+    <main>
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Hello, Bruno!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd'th' MMMM")}
+        </p>
+      </div>
+      <div className="px-5 mt-6">
+        <Search />
+      </div>
+    </main>
+  );
 }
